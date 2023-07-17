d717a32263ea53e24fcae7e8db0fbb271e46e477
Harjeetsingh123 <135020640+Harjeetsingh123@users.noreply.github.com>
2023-07-17 12:45:39 +0530

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <header>
+      <h1>React Photo Album | Columns Layout</h1>
+      <a
+        href="https://github.com/igordanchenko/react-photo-album"
+        target="_blank"
+        rel="noreferrer noopener"
+      ></a>
+      <a
+        href="https://react-photo-album.com/"
+        target="_blank"
+        rel="noreferrer noopener"
+      ></a>
+    </header>
+    <main>
+      <App />
+    </main>
+  </React.StrictMode>
+);
